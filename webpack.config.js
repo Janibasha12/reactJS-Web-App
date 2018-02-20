df6123aa51d527ddfedd5e6d919f6ea6880a0f77
Janibasha12 <basha.sk7891@GMAIL.COM>
2018-02-20 10:04:31 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/entry.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader"
+                }
+
+            }
+        ]
+
+    }
+};
